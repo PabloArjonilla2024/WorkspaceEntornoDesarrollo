d6f5bf9a5b225c62087e679644ec1e7c447b3377
PabloArjonilla2024 <parjcua165@g.educaand.es>
2024-10-25 13:53:57 +0200

    aaaaa

---
+++ ProjectoGit/src/ProjectoGit/Github.java
@@ -0,0 +1,14 @@
+package ProjectoGit;
+
+public class Github {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean a = true;
+		int b = 1;
+		while(a) {
+			System.out.println("A"+ b++);
+		}
+	}
+
+}
